aa27e28cff75006f9f1ece19e40009accc92fe45
Mauricio DZ <mauriciozanellaa9@gmail.com>
2023-03-15 21:10:31 -0300

    Primeiro commit
    Upload de pastas login e css

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" > </link>
+    <link rel = "stylesheet" href = "css/styles.css"></link>
+</head>
+<body>
+    <div id = "login-container">
+        <h1>LOGIN</h1>
+        <form action = "">
+            <label for ="email">E-mail</label>
+        <input type ="email" name = "email" id = "email" placeholder="Digite seu e-mail" autocomplete="off" >
+        </form>
+    </div>
+</body>
+</html>
