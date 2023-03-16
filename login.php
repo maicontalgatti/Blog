b2039090d73b1ff77f468713d27750f67c7489e8
Mauricio DZ <mauriciozanellaa9@gmail.com>
2023-03-15 21:51:48 -0300

    Fim de aula
    Criação de botões

--- login.php
+++ login.php
@@ -14,7 +14,19 @@
         <form action = "">
             <label for ="email">E-mail</label>
         <input type ="email" name = "email" id = "email" placeholder="Digite seu e-mail" autocomplete="off" >
-        </form>
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" placeholder="Digite sua senha">
+        <a href="#" id="forgot-pass">Esqueceu a senha?</a>
+        <input type="submit" value = "Login">
+    </form>
+        <div id = "social-container">
+            <p> Ou entre usando suas redes sociais</p>
+            <i class="fa fa-facebook-f"></i>
+            <i class="fa fa-linkedin"></i>
+
+
+        </div>
+   
     </div>
 </body>
 </html>
