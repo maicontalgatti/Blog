953fc154e0eaafbac889c6024a3ba321f82a04ec
Ampla Automacao <dionestalgattimaicon@gmail.com>
2023-03-23 08:50:30 -0300

    inicio feed

--- feed.php
+++ feed.php
@@ -2,9 +2,36 @@
 
 <div class="display feed border">
 
+    <?php
+    include("db.php");
+    include("mostrarerros.php");
+
+    $sql = "SELECT id, idusuario, titulo, texto, nomeusuario FROM blog.posts";
+    $result = $conn->query($sql);
+    // Verifica se há resultados
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+
+            echo '
+            <div class="postagem border">
+                <div class="titulopost border">
+                <p style="margin:0px"> ' . $row['nomeusuario'] . ' - ' . $row['titulo'] . ' </p> 
+                </div>
+                <div class="textopost border">
+                    <p style="margin:0px">' . $row['texto'] . '</p>
+                </div>
+            </div>';
+        }
+        echo "</table>";
+    } else {
+        echo "Não há resultados.";
+    }
+    // Fecha a conexão com o banco de dados
+    $conn->close();
+
+    ?>
 
 
-right
 
 </div>
 
