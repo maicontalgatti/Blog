3a71ac2fc878727e82f21d310e43b28235f144d1
Mauricio DZ <mauriciozanellaa9@gmail.com>
2023-03-22 21:36:38 -0300

    Estilização em css
    Estilizadas as partes dos botões da barra lateral

--- barra_lateral.php
+++ barra_lateral.php
@@ -1,8 +1,19 @@
 <html>
 
+<style>
+/*.display{
+    display: inline-block;
+    width: -300px;
+    height: 300px;
+}
+*/
+
+</style>
 <div class="display barraesquerda border"> 
+
                 
-left
+    <button class = "btn" onclick="teste()">Publicar</button>
+    <button class = "btn2" onclick="teste()">Sair</button>
 
 </div>
 
