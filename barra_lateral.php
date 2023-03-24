334d5e8ef265c7b79c63a10b3e7412bf1caa0da4
Ampla Automacao <dionestalgattimaicon@gmail.com>
2023-03-24 14:04:59 -0300

    barra up

--- barra_lateral.php
+++ barra_lateral.php
@@ -2,6 +2,10 @@
 
 <div class="display barraesquerda border" style='display:block!important;padding:15px'>
 
+<div>
+        <button type="button" class="btn" onclick="pagina_inicial()">Página inicial</button>
+    </div>
+    <br>
     <div>
         <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo" id='btn-postagem'>Publicar</button>
     </div>
@@ -9,7 +13,6 @@
     <div>
         <button type="button" class="btn" onclick="sair()">Sair</button>
     </div>
-
     <br>
     <div>
         <button type="button" class="btn" onclick="perfil()">Perfil</button>
@@ -23,8 +26,12 @@
     function sair() {
         window.location.href = "login.php";
     }
+
     function perfil() {
         window.location.href = "initial_page.php?modulo=perfil";
+    }
+    function pagina_inicial() {
+        window.location.href = "initial_page.php";
     }
 </script>
 
